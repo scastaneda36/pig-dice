5d6c291ed0280cb559ec72749640ba9619c77f8e
Sandra Castaneda and Stuart Gill <stuart@>
2018-10-31 16:09:18 -0700

    Create several functioning prototypes and constructors, but full program doesnt work yet

---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+// Business Logic for Consortium ---------
+function Consortium () {
+  this.players=[],
+  this.currentId=0
+}
+
+Consortium.prototype.addPlayer = function(player) {
+  player.id = this.assignId();
+  this.players.push(player);
+}
+
+Consortium.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Consortium.prototype.findPlayer = function(id) {
+  for (var i=0; i< this.players.length; i++) {
+    if (this.players[i]) {
+      if (this.players[i].id == id) {
+        return this.players[i];
+      }
+    }
+  };
+  return false;
+}
+
+function Player(name, score) {
+  this.name = name,
+  this.score= 0
+}
+
+Player.prototype.hold = function (localScore) {
+  this.score += localScore;
+  localScore=0;
+  return localScore;
+  switchPlayers
+}
+
+Player.prototype.switch = function (){
+
+}
+
+Player.prototype.startOver = function () {
+  this.name = "";
+  this.score = 0;
+}
+
+
+var rollDice = function() {
+  var localScore=0;
+  var rolling=Math.ceil(Math.random()*6);
+  window.rolling=rolling;
+  return rolling;
+  }
+
+  var total=0;
+
+function turnTotal () {
+  return total+=rolling;
+  }
+
+// User Interface Logic ---------
+// var addressBook = new Consortium();
+
+// function showContact(contactId) {
+//   var contact = addressBook.findContact(contactId);
+//   $("#show-contact").show();
+//   $(".player1-name").html(player.name);
+//   $(".player1-score").html(player.score);
+//   $(".player2-score").html(player.score);
+//
+
+var consortium = new Consortium();
+
+$(document).ready(function() {
+  $("form#player1").submit(function(event) {
+    event.preventDefault();
+    var inputtedPlayer1 = $("input#new-player1").val();
+    var newPlayer1 = new Player(inputtedPlayer1,0);
+    $(".player1-name").text(inputtedPlayer1);
+    consortium.addPlayer(newPlayer1);
+  });
+
+  $("form#player2").submit(function(event) {
+    event.preventDefault();
+    var inputtedPlayer2 = $("input#new-player2").val();
+    var newPlayer2 = new Player(inputtedPlayer2,0);
+    $(".player2-name").text(inputtedPlayer2);
+    consortium.addPlayer(newPlayer2);
+    });
+
+  $("#roll").on("click", function() {
+    $(".turn-roll").text(rollDice());
+    $(".turn-total").html(turnTotal());
+
+    // $(".player2-score").show();
+  });
+  // $("#hold").on("click", function() {
+  //   player.hold();
+  //   player.switch();
+  // });
+  // $("#reset").on("click", function() {
+  //   player.startOver();
+  // });
+  // //event.preventDefault();
+  // })
+
+
+  // $("#buttons").on("click", ".deleteButton", function() {
+  //   addressBook.deleteContact(this.id);
+  //   $("#show-contact").hide();
+  //   displayContactDetails(addressBook);
+  //   });
+  });
+
+    // var newAddress = new Address(inputtedHomeAddress, inputtedWorkAddress);
+    // var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newAddress);
